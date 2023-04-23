89999a6aa982dcf1b1f3a247213c60d9e81fb583
BrivaHamisi <brivahamisi@gmail.com>
2023-04-23 21:54:03 +0300

    Stable code

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -359,22 +359,6 @@
             </ul>
         </div>
     </nav>
-{{--        <div class="sidebar-body">--}}
-{{--            <a href="#" class="settings-sidebar-toggler">--}}
-{{--                <i data-feather="settings"></i>--}}
-{{--            </a>--}}
-{{--            <div class="theme-wrapper">--}}
-{{--                <h6 class="text-muted mb-2">Light Theme:</h6>--}}
-{{--                <a class="theme-item" href="../demo1/dashboard.html">--}}
-{{--                    <img src="../assets/images/screenshots/light.jpg" alt="light theme">--}}
-{{--                </a>--}}
-{{--                <h6 class="text-muted mb-2">Dark Theme:</h6>--}}
-{{--                <a class="theme-item active" href="../demo2/dashboard.html">--}}
-{{--                    <img src="../assets/images/screenshots/dark.jpg" alt="light theme">--}}
-{{--                </a>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-    </nav>
     <!-- partial -->
 
     <div class="page-wrapper">
